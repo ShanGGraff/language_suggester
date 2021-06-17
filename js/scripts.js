9200173bcdc25bf120f2a892a2a4dfcb09196d05
Shane Graff <copellius@gmail.com>
2021-06-17 16:18:37 -0700

    Add remaining branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
   $("form#language").submit(function(event) {
     const scriptingCompiled = $("#scriptingCompiled").val();
     console.log(scriptingCompiled);
-    const coffeeTea = $("#coffeeTea").val();
-    console.log(coffeeTea);
+    const coffee = $("#coffee").val();
+    console.log(coffee);
     const gophers =  $("#gophers").val();
     console.log(gophers);
     const monty = $("#monty").val();
@@ -14,12 +14,14 @@ $(document).ready(function() {
     const semiColons = $("#semiColons").val();
     console.log(semiColons);
 
-  if (scriptingCompiled === "Scripting" && coffeeTea !== "Coffee" && gophers !== "Yes" && monty === "Yes" && semiColons !== "Yes") {
+  if (scriptingCompiled === "Scripting" && coffee === "No" && gophers === "No" && monty === "Yes" && semiColons === "No") {
     alert("May we suggest Python");
-  } else if (scriptingCompiled === "Compiled" && coffeeTea === "Coffee" && gophers !== "Yes" && monty !== "Yes" && semiColons === "Yes") {
-    alert("May we suggest Java");
+  } else if (scriptingCompiled === "Compiled" && coffee === "Yes" && gophers === "No" && monty === "No" && semiColons === "Yes") {
+    alert("May we suggest Java"); 
+  } else if (scriptingCompiled === "Compiled" && coffee === "No" && gophers === "Yes" && monty === "No" && semiColons === "Yes") {
+    alert("May we suggest Go");
   } else {
-    alert("Continue On");
+    alert("We saddly have no suggestions for you");
   }
 
     // $("#output").text(result);
