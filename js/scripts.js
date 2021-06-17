b70131d94242f144cba7ed52b352be7d7fc2b28a
Shane Graff <copellius@gmail.com>
2021-06-17 15:39:43 -0700

    WIP: Add first branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,26 @@
 //UserInterface Logic
 $(document).ready(function() {
   $("form#language").submit(function(event) {
-    const scriptingCompiled = $("#scriptingCompiled").val().toUpperCase();
+    const scriptingCompiled = $("#scriptingCompiled").val();
     console.log(scriptingCompiled);
-    const coffeeTea = $("#coffeeTea").val().toUpperCase();
+    const coffeeTea = $("#coffeeTea").val();
     console.log(coffeeTea);
-    const gophers =  $("#gophers").val().toUpperCase();
+    const gophers =  $("#gophers").val();
     console.log(gophers);
-    const monty = $("#monty").val().toUpperCase();
+    const monty = $("#monty").val();
     console.log(monty);
-    const semiColons = $("#semiColons").val().toUpperCase();
+    const semiColons = $("#semiColons").val();
     console.log(semiColons);
+
+  if (scriptingCompiled === "Scripting" && coffeeTea !== "Coffee" && gophers !== "Yes" && monty === "Yes" && semiColons !== "Yes") {
+    alert("May we suggest Python");
+  } else if (scriptingCompiled === "Compiled" && coffeeTea === "Coffee" && gophers !== "Yes" && monty !== "Yes" && semiColons === "Yes") {
+    alert("May we suggest Java");
+  } else {
+    alert("Continue On");
+  }
+
+    // $("#output").text(result);
 
     event.preventDefault();
   });
