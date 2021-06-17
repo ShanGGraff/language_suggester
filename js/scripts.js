565fb22412ef5835345c4bfaa9132f28b98c433b
Shane Graff <copellius@gmail.com>
2021-06-17 11:53:24 -0700

    Add const to scripts.js to catch form input

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,11 @@
 //UserInterface Logic
 $(document).ready(function() {
   $("form#language").submit(function(event) {
-   
+    const scriptingCompiled = toUpperCase($("#scriptingCompiled").val());
+    const coffeeTea = toUpperCase($("#coffeeTea").val());
+    const gophers = toUpperCase($("#gophers").val());
+    const smonty = toUpperCase($("#monty").val());
+    const semiColons = toUpperCase($("#semiColons").val());
 
     event.preventDefault();
   });
